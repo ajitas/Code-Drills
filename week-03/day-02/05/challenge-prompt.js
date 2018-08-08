d7450281c71bb4e02127f8d7921271c2a81258c8
Ajita Srivastava <ajitas4000@gmail.com>
2018-08-07 23:49:54 -0700

    adding week3-day2-05

--- week-03/day-02/05/challenge-prompt.js
+++ week-03/day-02/05/challenge-prompt.js
@@ -7,15 +7,59 @@
 function anagramCheck (str1, str2) {
 
   // ---------- Your Code Here ----------
+var obj1 = {};
+for(var i = 0;i<str1.length;i++){
+  var letter = str1[i].toLowerCase();
+  if(letter !== " "){
+    if(!obj1[letter])
+      obj1[letter] = 1;
+    else
+      obj1[letter]++;
+  }
+}
+var obj2 = {};
+for(var i = 0;i<str2.length;i++){
+  var letter = str2[i].toLowerCase();
+  if(letter !== " "){
+    if(!obj2[letter])
+      obj2[letter] = 1;
+    else
+      obj2[letter]++;
+  }
+}
+
+return isEquivalent(obj1, obj2);
 
 
+function isEquivalent(a, b){
+  var aProps = Object.getOwnPropertyNames(a);
+    var bProps = Object.getOwnPropertyNames(b);
 
+    // If number of properties is different,
+    // objects are not equivalent
+    if (aProps.length != bProps.length) {
+        return false;
+    }
 
+    for (var i = 0; i < aProps.length; i++) {
+        var propName = aProps[i];
+
+        // If values of same property are not equal,
+        // objects are not equivalent
+        if (a[propName] !== b[propName]) {
+            return false;
+        }
+    }
+
+    // If we made it this far, objects
+    // are considered equivalent
+    return true;
+}
+}
 
 
   // ----------- End Code Area -----------
 
-}
 
 // This should console log true
 console.log(anagramCheck("I am Lord Voldemort", "Tom Marvolo Riddle"));
